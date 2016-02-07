659027081870756b6d002f76651749283f8e3de1
lylynguyen <lylynguyen812@gmail.com>
2016-02-06 17:29:47 -0800

    worked on create bill form

--- index.ios.js
+++ index.ios.js
@@ -18,19 +18,19 @@ import React, {
   DatePickerIOS,
 } from 'react-native';
 
-var Form = require('react-native-form')
-
 var ScrollableTabView = require('react-native-scrollable-tab-view');
 
 var messages = [{name: "Lyly", text: "a;lshee hksehkahfj askhfakjse ashkjehakjes afeksjfhk esksjks jfhskfe"},{name: "Boner", text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit."},{name: "Joey", text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit."},{name: "Nick", text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit."}]
 var finance = [{bill: "Rent", total: "$200.00"}, {bill: "Electric", total: "$50.00"}];
 
-var bills = [{username: 'lyly', payer: 'Nick',total: 200, name: 'rent', duedate: '2016-03-03', datepaid: null}, {username: 'Justin', payer: 'Nick', total: 200, name: 'water', duedate: '2016-03-04', datepaid: null} ];
+var bills = [{total: 200, name: 'rent', date: '2016-03-03'}, {total: 200, name: 'water', date: '2016-03-04'} ];
 
-var billHistory = [{username: 'lyly', payer: 'Nick',total: 200, name: 'rent', duedate: '2016-03-03', datepaid: 'completed'}, {username: 'Justin', payer: 'Nick', total: 200, name: 'water', duedate: '2016-03-04', datepaid: 'completed'}];
+var billHistory = [{username: 'lyly', payer: 'Nick',total: 200, name: 'rent', date: '2016-03-03', datepaid: 'completed'}, {username: 'Justin', payer: 'Nick', total: 200, name: 'water', date: '2016-03-04', datepaid: 'completed'}];
 
-var paymentOwed = [{username: 'lyly', payee: 'Nick',total: 200, name: 'rent', duedate: '2016-03-03', datepaid: null}, {username: 'Justin', payee: 'Nick', total: 200, name: 'water', duedate: '2016-03-04', datepaid: null}];
+var payments = [{username: 'lyly', payee: 'Nick',total: 200, name: 'rent', date: '2016-03-03', datepaid: null}, {username: 'Justin', payee: 'Nick', total: 200, name: 'water', date: '2016-03-04', datepaid: null}];
 
+
+var users = ["Joey", "Justin", "Nick", "Lyly"];
 
 var border = function(color) {
   return {
@@ -43,8 +43,7 @@ var App = React.createClass({
   getInitialState: function() {
     return {
       messages: messages,
-      finance: finance,
-      bills: bills
+      finance: finance
     }
   },
   render: function() {
@@ -54,14 +53,13 @@ var App = React.createClass({
         <MessageContainer messages={this.state.messages} />*/}
         <ScrollableTabView>
           <MessageContainer messages={this.state.messages} tabLabel="Messages" />
-          <FinanceContainer finance={this.state.finance} tabLabel="Finance" />
+          <FinanceContainer finance={this.state.finance} tabLabel='Finance'/>
         </ScrollableTabView>
       </View>
     )
   }
 });
 
-//<BillContainer bills={this.state.bills} tabLabel="Create Bill"/>
 
 var MessageForm = React.createClass({
   getInitialState: function() {
@@ -93,7 +91,7 @@ var MessageForm = React.createClass({
           onChangeText={(text) => this.setState({text})}
           value={this.state.text}
           rejectResponderTermination={false}
-          style={styles.textInput}
+          style={[styles.textInput]}
         />
         <View style={[styles.buttonContainer, border('red')]}>
           {this.sendMessageButton()}
@@ -133,7 +131,7 @@ var MessageContainer = React.createClass({
   render: function() {
     return (
       <View style={[styles.messageContainer, border('red')]}>
-        <Text style={styles.viewTitle}>Messages</Text>
+        <Text style={[styles.viewTitle]}>Messages</Text>
         <ListView
           dataSource={this.state.dataSource}
           renderRow={this.renderMessageEntry}
@@ -146,232 +144,374 @@ var MessageContainer = React.createClass({
 
 var BillContainer = React.createClass({
   getInitialState: function() {
-    return{
-
-      username: '',
-      name: '',
-      payer: '',
-      total: '',
-      duedate: '2016-03-16',
-      datepaid: ''
-    }
-  },
-
-  addBill: function() {
-    var billObject = {username: this.state.username, payer: this.state.payer, total: this.state.total, name: this.state.name, duedate: this.state.duedate, datepaid: null};
-    this.props.sendBill(billObject);
-  },
-
-   sendBillButton: function() {
-    return <TouchableHighlight
-      underlayColor="gray"
-      onPress={this.addBill}
-      style={[styles.sendMessageButton]}
-      >
-      <Text>
-        Send Bill
-      </Text>
-    </TouchableHighlight>
-  },
-
-  render: function() {
-    return  (
-      <View style={[styles.formTest, border('blue')]}>
-        <Form ref="form">
-        <TextInput name="username" placeholder="Name, email, phone number"
-        keyboardType='default'
-        style={{height: 40, paddingLeft: 10, textAlign: 'justify'}}/>
-      </Form>
-         <Text>Bill: </Text>
-        <TextInput
-          onChangeText={(name) => this.setState({name})}
-          value={this.state.name}
-          style={styles.textInput}
-        />
-         <Text>Payer: </Text>
-        <TextInput
-          onChangeText={(payer) => this.setState({payer})}
-          value={this.state.payer}
-          style={styles.textInput}
-        />
-        <Text>Total: </Text>
-        <TextInput
-          onChangeText={(total) => this.setState({total})}
-          value={this.state.total}
-          style={styles.textInput}
-        />
-        <Text>Payer: </Text>
-        <TextInput
-          onChangeText={(duedate) => this.setState({duedate})}
-          value={this.state.duedate}
-          style={styles.textInput}
-        />
-        <View style={[styles.buttonContainer, border('red')]}>
-          {this.sendBillButton()}
-        </View>
-      </View>
-    )
-  },
-});
-
-var BillEntry = React.createClass({
-  getInitialState: function() {
     var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     return {
       bills: bills,
       dataSource: ds.cloneWithRows(bills)
-    };
-  },
-  sendBill: function(billObj) {
-    this.state.bills.push(billObj);
-    this.setState({
-      dataSource:this.state.dataSource.cloneWithRows(this.state.bills),
-      bills: this.state.bills
-    })
-  },
-
-  renderBillEntry: function(rowData) {
-    return (
-      <View style={[styles.messagesEntry, border('blue')]}>
-        <TouchableHighlight
-          underlayColor="gray"
-          onPress={this.payBill}
-          >
-        <Text>{rowData.payer} owes
-        {rowData.username}
-        {rowData.total} for
-        {rowData.name} due on {rowData.duedate} </Text>
-        </TouchableHighlight>
-      </View>
-    )
-  },
-
-  render: function() {
-    return (
-      <View style={[styles.messageEntry, border('black')]}>
-        <ListView
-          dataSource={this.state.dataSource}
-          renderRow={this.renderBillEntry}
-        />
-      </View>
-    )
-  }
-  //need to move the submit/send bill function to pay/request container
-})
-
-var FinanceContainer = React.createClass({
-    getInitialState: function() {
-    return{
-      username: '',
-      name: '',
-      payer: '',
-      total: '',
-      duedate: '2016-03-16',
-      datepaid: ''
     }
   },
 
-  addBill: function() {
-    var billObject = {username: this.state.username, payer: this.state.payer, total: this.state.total, name: this.state.name, duedate: this.state.duedate, datepaid: null};
-    this.props.sendBill(billObject);
-  },
+  render: function () {
+    return (
+      <View style={[styles.messageContainer, border('red')]}>
+       <ListView
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) => (
+          <View>
+            <Text>
+              {rowData.name}
+            </Text>
+            <Text>
+              {rowData.total}
+            </Text>
+          </View>
+        )}
+       />
+      </View>
+    );
+  }
+});
 
-   sendBillButton: function() {
-    return <TouchableHighlight
-      underlayColor="gray"
-      onPress={this.addBill}
-      style={[styles.sendMessageButton]}
-      >
-      <Text>
-        Send Bill
-      </Text>
-    </TouchableHighlight>
+var PaymentContainer = React.createClass({
+  getInitialState: function() {
+    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
+    return {
+      payments: payments,
+      dataSource: ds.cloneWithRows(payments)
+    };
   },
 
   render: function() {
     return (
-      <View style ={[styles.messageContainer, border('black')]}>
-        <Text style={styles.viewTitle}> Bills</Text>
-        <View>
-          <BillEntry />
-        </View>
-        <Text style={styles.viewTitle}> Payment Owed to You</Text>
-        <View>
-          <PaymentOwedEntry />
-        </View>
+      <View style={[styles.messageContainer, border('red')]}>
+       <ListView
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) => (
+          <View>
+            <Text>
+              {rowData.username}
+            </Text>
+            <Text>
+              {rowData.total}
+            </Text>
+          </View>
+        )}
+       />
+      </View>
+    );
+  }
+});
 
-        <View>
-         <Text>Bill:</Text>
-        <TextInput
-          onChangeText={(name) => this.setState({name})}
-          value={this.state.name}
-          style={styles.textInput}
-        />
-         <Text>Payer: </Text>
-        <TextInput
-          onChangeText={(payer) => this.setState({payer})}
-          value={this.state.payer}
-          style={styles.textInput}
-        />
-        <Text>Total: </Text>
-        <TextInput
-          onChangeText={(total) => this.setState({total})}
-          value={this.state.total}
-          style={styles.textInput}
-        />
-        <Text>Payer: </Text>
-        <TextInput
-          onChangeText={(duedate) => this.setState({duedate})}
-          value={this.state.duedate}
-          style={styles.textInput}
-        />
-        <View style={[styles.buttonContainer, border('red')]}>
-          {this.sendBillButton()}
+
+var FinanceContainer = React.createClass({
+  getInitialState: function() {
+    return {
+      bills: bills,
+      payments: payments,
+      showBills: true,
+      showPayments: false,
+      showCreateBill: false
+    }
+  },
+  toggleBills: function() {
+    this.state.showPayments ? this.setState({showPayments: false}) : this.setState({showCreateBill: false})
+    this.setState({showBills: !this.state.showBills})
+  },
+
+  togglePayments: function() {
+    this.state.showBills ?  this.setState({showBills: false}) : this.setState({showCreateBill: false})
+    this.setState({showPayments: !this.state.showPayments})
+  },
+  toggleCreateBills: function() {
+    this.state.showPayments ? this.setState({showPayments: false}) : this.setState({showBills: false})
+    this.setState({showCreateBill: !this.state.showCreateBill})
+  },
+
+ viewBillsButton: function() {
+  return <TouchableHighlight underlayColor="gray"
+    onPress={this.toggleBills}
+  >
+    <Text>Bills</Text>
+    </TouchableHighlight>
+ },
+
+ viewPaymentsButton: function() {
+  return <TouchableHighlight underlayColor="gray"
+    onPress={this.togglePayments}
+  >
+    <Text>Payments</Text>
+    </TouchableHighlight>
+ },
+
+  viewCreateBillButton: function() {
+  return <TouchableHighlight underlayColor="gray"
+    onPress={this.toggleCreateBills}
+  >
+    <Text>Create Bill</Text>
+    </TouchableHighlight>
+ },
+
+ renderPayments: function() {
+  if(this.state.showPayments){
+    return (
+      <PaymentContainer/>
+    )
+  }
+ },
+
+  // loadPayments: function() {
+  //   fetch(process.env.Base_URL + '/payment/owed', {
+  //     method: 'GET',
+  //     headers: {
+  //       'Accept': 'application/json',
+  //       'Content-Type': 'application/json',
+  //       'token': AsyncStorage.getItem('obie')
+  //     }
+  //   })
+  //   .then(function(payments) {
+  //     this.setState({paymentsOwed: payments});
+  //   })
+  // },
+
+ renderBills: function() {
+  if(this.state.showBills) {
+    return (
+    <BillContainer/>
+    )
+  }
+ },
+
+//  loadBills: function() {
+//   fetch(process.env.Base_URL + '/payment/pay', {
+//     method: 'GET',
+//     headers: {
+//       'Accept': 'application/json',
+//       'Content-Type': 'application/json',
+//       'token': AsyncStorage.getItem('obie')
+//     }
+//   })
+//   .then(function(bills) {
+//     this.state.bills = bills; 
+//     this.setState({bills: this.state.bills});
+//   })
+// },
+
+ renderCreateBill: function() {
+  if(this.state.showCreateBill){
+    return (
+      <CreateBill/>
+    )
+  }
+ },
+
+  render: function() {
+    return (
+      <View style={[styles.finContainer, border('orange')]}>
+        <View style={[styles.finBillPayContainer, border('purple')]}>
+        {this.renderBills()}
+        {this.renderPayments()}
+        {this.renderCreateBill()}
         </View>
+        <View style={[styles.finNavButtons, border('red')]}>
+          <View>
+            {this.viewBillsButton()}
+          </View>
+          <View>
+            {this.viewPaymentsButton()}
+          </View>
+          <View>
+            {this.viewCreateBillButton()}
+          </View>
         </View>
       </View>
     )
   }
 });
 
-var PaymentOwedEntry = React.createClass({
-  getInitialState: function() {
-    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+var DatePickerExample = React.createClass({
+  getDefaultProps: function () {
     return {
-      paymentOwed: paymentOwed,
-      dataSource: ds.cloneWithRows(paymentOwed)
+      date: new Date(),
+      timeZoneOffsetInHours: (-1) * (new Date()).getTimezoneOffset() / 60,
     };
   },
-  render: function () {
+
+  getInitialState: function() {
+    return {
+      date: this.props.date,
+      timeZoneOffsetInHours: this.props.timeZoneOffsetInHours,
+    };
+  },
+
+  onDateChange: function(date) {
+    this.setState({date: date});
+    gDate = this.state.date;  
+  },
+
+  onTimezoneChange: function(event) {
+    var offset = parseInt(event.nativeEvent.text, 10);
+    if (isNaN(offset)) {
+      return;
+    }
+    this.setState({timeZoneOffsetInHours: offset});
+  },
+
+  render: function() {
     return (
-      <View style={[styles.messagesEntry, border('blue')]}>
-        <ListView
-          dataSource={this.state.dataSource}
-          renderRow={(rowData) => <Text>{rowData.username} owes {rowData.username} {rowData.total} for {rowData.name} due on {rowData.duedate}</Text>}
-        />
+      <View style={styles.container}>
+        <View>
+          <DatePickerIOS
+            date={this.state.date}
+            mode="datetime"
+            timeZoneOffsetInMinutes={this.state.timeZoneOffsetInHours * 60}
+            onDateChange={this.onDateChange}
+            minuteInterval={30}
+            style={[{height: 320}]}
+          />
+        </View>
+        <View
+          style={{justifyContent: 'flex-end', alignItems: 'center', marginBottom: 30}}
+          >
+          <Text onPress={this.props.toggleClose}>Close</Text>
+        </View>
       </View>
+    );
+  },
+});
+
+var CreateBill = React.createClass({
+  getInitialState: function() {
+    return {
+      name:'',
+      total:'',
+      Date:'',
+      showDate: false,
+      showCustomSplit: false,
+      users: users,
+    }
+  },
+
+  toggleDate: function() {
+    this.setState({showDate: !this.state.showDate})
+  },
+
+  renderDate: function() {
+    if(this.state.showDate) {
+      return <DatePickerExample/>
+    }
+  },
+  // addBill: function(bill) {
+  //   fetch(process.env.Base_URL + '/payment/bill', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Accept': 'application/json',
+  //       'Content-Type': 'application/json',
+  //       'token': AsyncStorage.getItem('obie')
+  //     },
+  //     body: JSON.stringify(bill)
+  //   })
+  //   .then(function(id) {
+  //     this.createPayments(id)
+  //     this.loadBills();
+  //     socket.emit('bill');
+  //     })
+  // },
+  toggleCustomSplit: function() {
+    this.setState({showCustomSplit: !this.state.showDate})
+  },
+
+  renderCustomSplit: function() {
+    if(this.state.showCustomSplit) {
+      return <CustomSplitContainer/>
+    }
+  },
+
+  render: function() {
+    //Due date, on press need to render date picker
+    return (
+    <View>
+      <Text>Bill: </Text>
+        <TextInput
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.choreName}
+          rejectResponderTermination={false}
+          style={styles.textInput}/>
+        {this.renderDate()}
+      <Text>Total: </Text>
+        <TextInput
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.choreName}
+          rejectResponderTermination={false}
+          style={styles.textInput}/>
+        <TouchableHighlight onPress={this.toggleDate}>
+          <Text>Due Date</Text>
+        </TouchableHighlight>
+        <TouchableHighlight>
+          <Text>Split Evenly</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this.toggleCustomSplit}>
+          <Text>Custom Split</Text>
+        </TouchableHighlight>
+        {this.renderCustomSplit()}
+    </View>
     )
   }
 });
 
-var payBill; //function to pay 
+var CustomSplitContainer = React.createClass({
 
-
+  render: function() {
+  var userlist = users.map(function(user) {
+    return (
+      <View>
+      <Text>{user}: </Text>
+      <TextInput
+       // onChangeText={(userTotal) => this.setState({userTotal})}
+          // value={this.state.choreName}
+          rejectResponderTermination={false}
+          style={styles.textInput}
+      />
+      </View>
+    )
+  });
+    return (
+      <View>
+        <View>{userlist}</View>
+        <TouchableHighlight>
+          <Text>Submit</Text>
+        </TouchableHighlight>
+      </View>
+    )
+  }
+})
 
 const styles = StyleSheet.create({
+
+  finContainer: {
+    flex: 1,
+    alignItems: 'stretch'
+  },
+  finBillPayContainer: {
+    flex: 9,
+    alignItems: 'center'
+  },
+  finNavButtons: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
   container: {
     flex: 1,
     paddingTop: 25,
     justifyContent: 'center',
     alignItems: 'stretch',
     backgroundColor: 'white',
-
   },
   navbar: {
     flex: 2,
     justifyContent: 'center',
     alignItems: 'center',
-
 
   },
   messageContainer: {
@@ -380,6 +520,28 @@ const styles = StyleSheet.create({
     alignItems: 'stretch',
     backgroundColor: '#F5FCFF',
   },
+
+  form: {
+    flex: 3,
+    justifyContent: 'center',
+    alignItems: 'stretch'
+  },
+  formTest: {
+    justifyContent: 'center',
+    alignItems: 'stretch'
+  },
+  textInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: 280,
+    alignSelf: 'center'
+  },
+  buttonContainer: {
+    flex: 1,
+    alignItems: 'stretch'
+  },
+
   messageform: {
     flex: 3,
     justifyContent: 'center',
@@ -417,7 +579,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
-  },
+  }
 });
 
 AppRegistry.registerComponent('ObieObieOh', () => App);
